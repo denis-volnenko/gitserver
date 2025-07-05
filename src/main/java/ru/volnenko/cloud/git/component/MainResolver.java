397f988f592a3bbd5d307c0b2f7c3b9087ec6711
Denis Volnenko <denis@volnenko.ru>
2025-07-05 22:45:45 +0400

    Updated repo resolver.

--- src/main/java/ru/volnenko/cloud/git/component/MainResolver.java
+++ src/main/java/ru/volnenko/cloud/git/component/MainResolver.java
@@ -1,6 +1,7 @@
 package ru.volnenko.cloud.git.component;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
 import org.eclipse.jgit.lib.Repository;
@@ -8,19 +9,20 @@ import org.eclipse.jgit.transport.ServiceMayNotContinueException;
 import org.eclipse.jgit.transport.resolver.RepositoryResolver;
 import org.eclipse.jgit.transport.resolver.ServiceNotAuthorizedException;
 import org.eclipse.jgit.transport.resolver.ServiceNotEnabledException;
-import org.example.MemoryRepositoryWww;
 import ru.volnenko.cloud.git.exception.IncorrectRepositoryException;
 
 public final class MainResolver implements RepositoryResolver<HttpServletRequest> {
 
     @Override
     public Repository open(HttpServletRequest req, String name) throws RepositoryNotFoundException, ServiceNotAuthorizedException, ServiceNotEnabledException, ServiceMayNotContinueException {
-        final String url = req.getRequestURI();
-        final String[] parts = url.split(".git/");
+        @NonNull final String url = req.getRequestURI();
+        @NonNull final String[] parts = url.split(".git/");
         if (parts.length > 1) {
-            final DfsRepositoryDescription description = new DfsRepositoryDescription(url);
-            final String repo = parts[0];
+            @NonNull final DfsRepositoryDescription description = new DfsRepositoryDescription(url);
+            @NonNull final String repo = parts[0];
             System.out.println(repo);
+            @NonNull final RepositoryS3 repositoryS3 = new RepositoryS3(description);
+            return repositoryS3;
         }
         throw new IncorrectRepositoryException();
     }
