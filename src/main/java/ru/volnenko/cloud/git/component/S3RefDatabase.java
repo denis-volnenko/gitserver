df593fe9aeda46e9ae257bf579ac40b9cb9e8866
Denis Volnenko <denis@volnenko.ru>
2025-07-05 22:28:00 +0400

    Added refconfig.

--- src/main/java/ru/volnenko/cloud/git/component/S3RefDatabase.java
+++ src/main/java/ru/volnenko/cloud/git/component/S3RefDatabase.java
@@ -1,11 +1,44 @@
 package ru.volnenko.cloud.git.component;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 import org.eclipse.jgit.internal.storage.dfs.DfsReftableDatabase;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
+import org.eclipse.jgit.internal.storage.reftable.ReftableConfig;
 
 public final class S3RefDatabase extends DfsReftableDatabase {
 
+    @NonNull
+    private final ReftableConfig reftableConfig;
+
+    @Getter
+    @Setter
+    private boolean performsAtomicTransactions = true;
+
     public S3RefDatabase(DfsRepository repo) {
         super(repo);
+        @NonNull final ReftableConfig cfg = new ReftableConfig();
+        cfg.setAlignBlocks(false);
+        cfg.setIndexObjects(false);
+        cfg.fromConfig(repo.getConfig());
+        reftableConfig = cfg;
     }
+
+    @NonNull
+    public ReftableConfig createConfig() {
+        return reftableConfig;
+    }
+
+    @NonNull
+    @Override
+    public ReftableConfig getReftableConfig() {
+        return super.getReftableConfig();
+    }
+
+    @Override
+    public boolean performsAtomicTransactions() {
+        return this.performsAtomicTransactions;
+    }
+
 }
