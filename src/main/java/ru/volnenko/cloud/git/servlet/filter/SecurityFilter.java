d7af6b0ee070f1d4d29fd7e2daf643dbc62a9826
Denis Volnenko <denis@volnenko.ru>
2025-07-07 11:16:20 +0400

    Added index security access.

--- src/main/java/ru/volnenko/cloud/git/servlet/filter/SecurityFilter.java
+++ src/main/java/ru/volnenko/cloud/git/servlet/filter/SecurityFilter.java
@@ -18,6 +18,11 @@ public final class SecurityFilter implements Filter {
         @NonNull final HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         @NonNull final HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
 
+        if (httpServletRequest.getRequestURI().equals("/")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
         final String authHeader = httpServletRequest.getHeader("Authorization");
         final String username = SettingUtil.getSecurityUsername();
         final String password = SettingUtil.getSecurityPassword();
